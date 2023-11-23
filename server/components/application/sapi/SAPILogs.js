ba8690d05f939d107128f8afb82630a82f1d04d5
Larionov Alexandr <a.f.larionov@gmail.com>
2023-11-23 21:12:09 +0300

    - buyHealth method

--- server/components/application/sapi/SAPILogs.js
+++ server/components/application/sapi/SAPILogs.js
@@ -5,37 +5,26 @@ SAPILogs = function () {
     this.log = function (cntx, message, level, details, telega) {
 
         Logs.log(message, level, details, Logs.TYPE_CLIENT_DEBUG_INFO, telega);
-        //@todo-method
+
         Kafka.sendToCommon({message: message, level: "INFO", details: details},
             cntx && cntx.user ? cntx.user.id : undefined,
             Kafka.TYPE_LOG_RQ_DTO)
     };
 
-    this.clientLoaded = function (cntx, prid) {
-        //@todo to backlog move
-        pFinish(prid);
-    };
-
     this.sendUserAgent = function (cntx, userAgentString) {
-        //  DataUser.updateUserAgentString(cntx.user.id, userAgentString);
-        //@todo-method
         Kafka.sendToCommon({userAgentString: userAgentString}, cntx.user.id, Kafka.TYPE_SENDUSERAGENT_RQ_DTO)
     };
 
     this.showMoneyDialog = function (cntx, message) {
-        //@todo-method
         Kafka.sendToCommon({
             message: "Игрок " + cntx.user.socNetUserId + " Открыл диалог покупки голосов: " + message,
             level: "INFO",
         }, cntx.user.id, Kafka.TYPE_LOG_RQ_DTO);
-
-
-        //Logs.log("Проверка соощений об ошибках", Logs.LEVEL_DEBUG, null, null, true);
+        Logs.log("Игрок " + cntx.user.socNetUserId + " Открыл диалог покупки голосов: " + message, Logs.LEVEL_INFO, null, null, true);
     };
 
 
     this.closeMoneyDialog = function (cntx) {
-        //@todo-method
         Kafka.sendToCommon({
             message: "Игрок:" + cntx.user.socNetUserId + "  Закрыл диалог покупки голосов",
             level: "INFO"
@@ -44,7 +33,6 @@ SAPILogs = function () {
     };
 
     this.showStuffShopDialog = function (cntx, stuffFieldName) {
-        //@todo-method
         Kafka.sendToCommon({
             message: "Игрок:" + cntx.user.socNetUserId + " открыл покупку магии " + stuffFieldName,
             level: "INFO"
